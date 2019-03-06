2c300070eaf538c08719871f6e9d50fd469b13ac
Walter Lehner <wlehner@gmail.com>
2019-03-06 14:43:03 -0500

    More XML stuff

--- RNCScript.py
+++ RNCScript.py
@@ -21,11 +21,29 @@ corpusfiles = 'sample_ar/TEXTS/.*.xhtml'
 corpusfiction = XMLCorpusReader(fiction_root, fictionfiles)
 corpus = XMLCorpusReader(corpus_root, corpusfiles)
 
-testfiction = ET.parse(fiction_root + fictionfiles[1])
+verbs_of_change = ["стать","встать","поставить","лечь","класть",
+                    "положить","уложить","садиться","посадить","сесть",
+                    "усесться","вешать","повесить","вешаться",
+                    "повеситься","прятаться","спрятаться","прятать",
+                    "спрятать","собираться"]
 
-for word in testfiction.getroot()[1].iter('w'):
-    if word[0].get('gr') == 'PR':
-        print (word[0].get('lex'))
+#==============================================================================
+# testfiction = ET.parse(fiction_root + fictionfiles[1])
+# 
+# for word in testfiction.getroot()[1].iter('w'):
+#     if word[0].get('gr') == 'PR':
+#         print (word[0].get('lex'))
+#==============================================================================
+
+def processdirectory(directory):
+    filelist = os.listdir(directory)
+    for file in filelist:
+        filexml = ET.parse(directory + file)
+        for sentence in filexml.getroot()[1].iter('se'):
+            for word in sentence.iter('w'):
+                if word[0].get('lex') in verbs_of_change:
+                    #Put together a list of 
+                    print("found one")
 
 #testwords = testfiction.findall("w")
 
