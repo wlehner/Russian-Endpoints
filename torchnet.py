530c456e46b9889f06de827260c1fbfa328e70a3
Walter Lehner <wlehner@gmail.com>
2019-04-17 14:39:47 -0400

    Word2vec

--- torchnet.py
+++ torchnet.py
@@ -6,10 +6,10 @@ Created on Wed Apr  3 14:17:13 2019
 @author: walterlehner
 """
 from __future__ import print_function
-import torch
-import conllu
 from conllu import parse
-import os
+import os, gensim, torch, conllu
+from gensim.models import word2vec
+from gensim.models import KeyedVectors
 
 corpus_root= 'sample_ar/TEXTS'
 fiction_root= 'sample_ar/TEXTS/Fiction/'
@@ -18,27 +18,89 @@ file1= 'ru_syntagrus-ud-dev.conllu'
 file2= 'ru_syntagrus-ud-test.conllu'
 file3= 'ru_syntagrus-ud-train.conllu'
 
-#def constructconllu(sentence, objindex, prepindex):
-vocabulary = []
+prepositions = ["в","на","за","к","из","с","от"]
 
-def connllubiglist(file):
-    corpus = pars(open(changefilename, "w",encoding="utf-8"))
+
+#Takes Conllu File and Produces a List of Sentences as lists of Lemmas
+def conllulemlist(file):
+    result = []
+    corpus = parse(open(file, 'r',encoding ="utf-8").read())
     for sentence in corpus:
-        connlluprocess(sentence)
-        
-def connlluprocess(sentence):
-    example = []
-    example[0] = []
+        sentlist = []
+        for word in sentence:
+            sentlist.append(word["lemma"])
+        result.append(sentlist)
+    return result
+
+
+sentences = conllulemlist(file3)
+model = gensim.models.Word2Vec(sentences, min_count=1)
+model.save("word2vec.model")
+word_vectors = model.wv
+#del model
+
+#Takes Conllu Format and Produces a list of examples
+
+def processconllu(file):
+    corpus = parse(open(file, 'r',encoding ="utf-8").read())
+    examples = []
+    for sentence in corpus:
+        sentprep = []
+        for word in sentence:
+            if word['lemma'] in prepositions:
+                sentprep.append(word['lemma'])
+        if sentprep:
+            examples.append(processconlsent(sentence, sentprep))
+    return examples
+            
+def processconlsent(sentence, preplist):
+    pos = []
+    number = []
     for word in sentence:
-        base = word["lemma"]
-        if base not in biglist:
-            biglist.append(base)
-        example[0].append(biglist.index(base))
-                
-def xmlbiglist(file):
+#POS
+        if word['upostag']=='VERB':
+            pos.append(1)
+        elif word['upostag']== 'NOUN':
+            pos.append(2)
+        elif word['upostag']== 'ADJ':
+            pos.append(3)
+        elif word['upostag']== 'ADV':
+            pos.append(4)
+        elif word['upostag']== 'AUX':
+            pos.append(5)
+        elif word['upostag']== 'CCONJ':
+            pos.append(6)
+        elif word['upostag']== 'DET':
+            pos.append(7)
+        elif word['upostag']== 'INTJ':
+            pos.append(8)
+        elif word['upostag']== 'NUM':
+            pos.append(9)
+        elif word['upostag']== 'PART':
+            pos.append(10)
+        elif word['upostag']== 'PRON':
+            pos.append(11)
+        elif word['upostag']== 'PROPN':
+            pos.append(12)
+        elif word['upostag']== 'PUNCT':
+            pos.append(13)
+        elif word['upostag']== 'SCONJ':
+            pos.append(14)
+        elif word['upostag']== 'SYM':
+            pos.append(15)
+        elif word['upostag']== 'X':
+            pos.append(16)
+#NUMBER
+        if word['number']== 'Sing':
+            number.append(1)
+        elif word['number']== 'Plur':
+            number.append(2)
+        
     
-    
-#Construct Vocabulary from both corpuses
+        
+
+#vector = word_vectors["word"]
+#wv = KeyedVectors.load("model.wv", mmap='r')
 #Construct examples
 #   Go through the sentences in Connllu
 #       Use vocabulary to make list of words
