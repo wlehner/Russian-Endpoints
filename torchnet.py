4151fb4b54c69e44e68173c7387c10b16992a95c
Walter Lehner <wlehner@gmail.com>
2019-04-19 12:15:36 -0400

    Preparing Conllu Data

--- torchnet.py
+++ torchnet.py
@@ -57,44 +57,127 @@ def processconlsent(sentence, preplist):
     pos = []
     number = []
     for word in sentence:
-#POS
-        if word['upostag']=='VERB':
-            pos.append(1)
-        elif word['upostag']== 'NOUN':
-            pos.append(2)
-        elif word['upostag']== 'ADJ':
-            pos.append(3)
-        elif word['upostag']== 'ADV':
-            pos.append(4)
-        elif word['upostag']== 'AUX':
-            pos.append(5)
-        elif word['upostag']== 'CCONJ':
-            pos.append(6)
-        elif word['upostag']== 'DET':
-            pos.append(7)
-        elif word['upostag']== 'INTJ':
-            pos.append(8)
-        elif word['upostag']== 'NUM':
-            pos.append(9)
-        elif word['upostag']== 'PART':
-            pos.append(10)
-        elif word['upostag']== 'PRON':
-            pos.append(11)
-        elif word['upostag']== 'PROPN':
-            pos.append(12)
-        elif word['upostag']== 'PUNCT':
-            pos.append(13)
-        elif word['upostag']== 'SCONJ':
-            pos.append(14)
-        elif word['upostag']== 'SYM':
-            pos.append(15)
-        elif word['upostag']== 'X':
-            pos.append(16)
-#NUMBER
-        if word['number']== 'Sing':
-            number.append(1)
-        elif word['number']== 'Plur':
-            number.append(2)
+        pos.append(processpos(word))
+        
+def processpos(word):
+    if word['upostag']=='VERB':
+        return 1
+    elif word['upostag']== 'NOUN':
+        return 2
+    elif word['upostag']== 'ADJ':
+        return 3
+    elif word['upostag']== 'ADV':
+        return 4
+    elif word['upostag']== 'AUX':
+        return 5
+    elif word['upostag']== 'CCONJ':
+        return 6
+    elif word['upostag']== 'DET':
+        return 7
+    elif word['upostag']== 'INTJ':
+        return 8
+    elif word['upostag']== 'NUM':
+        return 9
+    elif word['upostag']== 'PART':
+        return 10
+    elif word['upostag']== 'PRON':
+        return 11
+    elif word['upostag']== 'PROPN':
+        return 12
+    elif word['upostag']== 'PUNCT':
+        return 13
+    elif word['upostag']== 'SCONJ':
+        return 14
+    elif word['upostag']== 'SYM':
+        return 15
+    elif word['upostag']== 'X':
+        return 16
+
+def processnum(word):
+    if word['feats']['Number']== 'Sing':
+        return 1
+    elif word['feats']['Number']== 'Plur':
+        return 2
+    else:
+        return 0
+
+def processcase(word):
+    if word['feats']['Case'] =='Acc':
+        return 1
+    elif word['feats']['Case'] =='Dat':
+        return 2
+    elif word['feats']['Case'] =='Gen':
+        return 3
+    elif word['feats']['Case'] =='Ins':
+        return 4
+    elif word['feats']['Case'] =='Loc':
+        return 5
+    elif word['feats']['Case'] =='Nom':
+        return 6
+    elif word['feats']['Case'] =='Par':
+        return 7
+    elif word['feats']['Case'] =='Voc':
+        return 8
+    else:
+        return 0
+
+def processgender(word):
+    if word['feats']['Gender']=='Fem':
+        return 1
+    elif word['feats']['Gender']=='Masc':
+        return 2
+    elif word['feats']['Gender']=='Neut':
+        return 3
+    else:
+        return 0
+    
+def processtense(word):
+    if word['feats']['Tense']=='Fut':
+        return 1
+    elif word['feats']['Tense']=='Past':
+        return 2
+    elif word['feats']['Tense']=='Pres':
+        return 3
+    else:
+        return 0
+
+def processaspect(word):
+    if word['feats']['Aspect']=='Imp':
+        return 1
+    elif word['feats']['Aspect']=='Perf':
+        return 2
+    else:
+        return 0
+
+def processmood(word):
+    if word['feats']['Mood']=='Imp':
+        return 1
+    if word['feats']['Mood']=='Ind':
+        return 2
+    else:
+        return 0
+
+def processperson(word):
+    if word['feats']['Person']=='1':
+        return 1
+    elif word['feats']['Person']=='2':
+        return 2
+    elif word['feats']['Person']=='3':
+        return 3
+    else:
+        return 0
+    
+def processverbform(word):
+    if word['VerbForm']=='':
+        return
+
+
+
+    
+    
+
+
+#TENSE
         
     
         
